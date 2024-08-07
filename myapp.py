f0ad14beff064422aeceeaba79f49ce98eb60645
ngxx-fus <msnp@outlook.com.vn>
2024-08-07 16:04:58 +0700

    v.2.1.41 trying to fix  FireWarningButton not exec

--- myapp.py
+++ myapp.py
@@ -336,7 +336,8 @@ class MYAPP(QWidget):
     def _FireWaring(self):
         print("[INFO] MYAPP._FireWaring: Creating thread3!")
         if hasattr(self, "thread3") == True:
-            if self.thread3.isRunning() == False():
+            if self.thread3.isRunning() == False:
+                self.thread3.exit()
                 print("[INFO] MYAPP._FireWaring: thread3 exist -> restart!")
                 self.thread3.exec()
             else:

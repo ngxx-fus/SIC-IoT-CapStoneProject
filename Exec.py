45aa31613468aa341a8efdbb8b0c90e8f63f8ca0
ngxx-fus <msnp@outlook.com.vn>
2024-08-07 12:50:26 +0700

    v.2.1.9 Fix dev bugs

--- Exec.py
+++ Exec.py
@@ -1,32 +1,31 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as IO
 from time import sleep
 
 class Exec:
     def __init__(self):
-        self.self.IO = GPIO
-        self.self.IO.setmode(self.IO.BCM)
-        #self.self.IO.setwarnings(False)
+        IO.setmode(IO.BCM)
+        #IO.setwarnings(False)
         self.BuzzerPin = 21
         self.LightPin  = 20
-        self.self.IO.setup(self.BuzzerPin, self.IO.OUT)
-        self.self.IO.setup(self.LightPin, self.IO.OUT)
+        IO.setup(self.BuzzerPin, IO.OUT)
+        IO.setup(self.LightPin, IO.OUT)
 
     def _HL(self, var):
         if var > 0.0:
-            return self.IO.HIGH
-        return self.IO.LOW
+            return IO.HIGH
+        return IO.LOW
 
     def BuzzerSet(self, state):
-        self.IO.output(self.BuzzerPin, self._HL(state))
+        IO.output(self.BuzzerPin, self._HL(state))
 
     def BuzzerSquaredPulse(self, time=1):
-        self.IO.output(self.BuzzerPin, self._HL(1))
+        IO.output(self.BuzzerPin, self._HL(1))
         sleep(time/2)
-        self.IO.output(self.BuzzerPin, self._HL(0))
+        IO.output(self.BuzzerPin, self._HL(0))
         sleep(time/2)
 
     def LightSet(self, state):
-        self.IO.output(self.LightPin, self._HL(state))
+        IO.output(self.LightPin, self._HL(state))
     
     def __del__(self):
-        self.IO.cleanup()
+        IO.cleanup()

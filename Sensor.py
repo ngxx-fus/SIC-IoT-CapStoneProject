2de41aa85a15911867e644ce5c1de717b94523c8
ngxx-fus <msnp@outlook.com.vn>
2024-08-08 13:35:59 +0700

    v.3.2.10

--- Sensor.py
+++ Sensor.py
@@ -20,18 +20,6 @@ class Sensor:
         self.GAS = False
         self.Flame = False
         
-        ###### setup pins ######
-        self.DTH11_OutPin = 26
-        self.GAS_OutPin = 16
-        IO.setup(self.DTH11_OutPin, IO.IN, pull_up_down=IO.PUD_DOWN)
-        IO.setup(self.GAS_OutPin, IO.IN, pull_up_down=IO.PUD_DOWN)
-        
-        ###### lambda functions ######
-        self.GetTemperature = lambda: self.Temp
-        self.GetHumidity    = lambda: self.Humid
-        self.GetGAS         = lambda: self.GAS
-        self.GetFlame       = lambda: self.Flame
-
     def Read(self):
         self.Temp, self.Humid = (randint(24, 33), randint(60,70))
         self.Flame = bool(randint(0,100) < 50)
